795a49970206f0864ddba7638423b2e122f5bcd0
İrem Çalmaz <iremcalmazz@gmail.com>
2022-01-10 12:13:14 +0300

    Delete IQueue.java

--- IQueue.java
+++
@@ -1,9 +0,0 @@
-
-public interface IQueue 
-{
-	public void enqueue(Object item) throws QueueFull;
-	public Object peek() throws QueueEmpty;
-	public Object dequeue() throws QueueEmpty;;
-	public boolean isEmpty();
-
-}
