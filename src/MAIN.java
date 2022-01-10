3e647a74ec71b74712d1f23b3985a4512f04d57a
İrem Çalmaz <iremcalmazz@gmail.com>
2022-01-10 12:16:37 +0300

    Add files via upload

---
+++ src/MAIN.java
@@ -0,0 +1,17 @@
+import java.awt.EventQueue;
+
+public class MAIN {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
